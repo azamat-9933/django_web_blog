9ffff0b40096d5eb069bc19763fbd364dc4c76ad
azamat-9933 <azamat.yakhyayeff@gmail.com>
2024-02-06 20:55:04 +0500

    base_html

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,24 @@ def index(request):
     categories = Category.objects.all()
     articles = Article.objects.all()
 
-
     context = {
         "title": "Главная страница",
         "categories": categories,
         "articles": articles
     }
 
-
     return render(request, "main_page.html", context)
+
+
+def category_page_view(request, category_id):
+    articles = Article.objects.filter(category=category_id)
+    trends = Article.objects.all().order_by('-views')
+
+    context = {
+        "title": f"Категория: {Category.objects.get(id=category_id).title}",
+        "articles": articles,
+        "trends": trends
+    }
+
+    return render(request, "category_page.html", context)
+
